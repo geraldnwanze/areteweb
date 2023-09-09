038638f30acdb232438466b3ed7be1e3f42eed61
Gerald Nwanze <nwanzegerald@gmail.com>
2023-09-09 21:55:04 +0100

    working on the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,8 +10,18 @@ const Hero = () => {
                     <div className="">
                         <h1 className="text-[52px] text-[#1D2939] font-extrabold leading-[100%]">It’s Easy to Find Your <br /> <span className="text-[#690068]">Dream Job</span></h1>
                         <p className="mt-10 text-[18px]">Each month, more than 3 million job seekers turn to <br /> website in their search for work, making over 140,000 <br /> applications every single day</p>
-                        <div className="bg-white h-20 rounded-2xl mt-16">
-                            
+                        <div className="bg-white h-20 rounded-2xl mt-16 flex items-center justify-between p-5">
+                            <div className="flex flex-col gap-2 border-r-2">
+                                <label htmlFor="">Select Job Type</label>
+                                <input type="text" placeholder="UI /UX Designer" />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <label htmlFor="">Location</label>
+                                <input type="text" placeholder="USA, Colorado" />
+                            </div>
+                            <div>
+                                <button className="bg-[#FEBA00] rounded-lg px-8 py-5 text-white text-[16px] font-[600]">Action</button>
+                            </div>
                         </div>
                     </div>
                 </div>
