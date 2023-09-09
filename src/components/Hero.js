463cd3039150798bbb16c7e286228a26d259aef2
Gerald Nwanze <nwanzegerald@gmail.com>
2023-09-09 21:38:51 +0100

    working on the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,22 @@
 import heroImage from "../images/hero-image.png";
+import heroVector from "../images/hero-vector.png";
 
 const Hero = () => {
     return (
         <section className="px-10">
-            <div className="flex justify-around">
-                <div>
-                    <div className="p-32">
-                        <h1 className="text-[52px] text-[#1D2939] font-extrabold">It’s Easy to Find Your <span className="text-[#690068]">Dream Job</span></h1>
-                        <p>Each month, more than 3 million job seekers turn to website in their search for work, making over 140,000 applications every single day</p>
+            <img src={heroVector} alt="" className="absolute mt-10" />
+            <div className="relative flex justify-around">
+                <div className="mt-56 px-10">
+                    <div className="">
+                        <h1 className="text-[52px] text-[#1D2939] font-extrabold leading-[100%]">It’s Easy to Find Your <br /> <span className="text-[#690068]">Dream Job</span></h1>
+                        <p className="mt-10 text-[18px]">Each month, more than 3 million job seekers turn to <br /> website in their search for work, making over 140,000 <br /> applications every single day</p>
+                        <div className="bg-white h-20 rounded-2xl mt-16">
+                            
+                        </div>
                     </div>
                 </div>
                 <div>
-                    <img src={heroImage} alt="" />
+                    <img src={heroImage} alt="" className="w-[400%]" />
                 </div>
             </div>
         </section>
