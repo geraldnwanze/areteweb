39088add1b40a75bb912e22d29993fb1e9300f70
Gerald Nwanze <nwanzegerald@gmail.com>
2023-10-15 13:28:07 +0100

    still working on the messages main page

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -6,9 +6,12 @@ import users from "../images/icons/users.svg";
 import jobs from "../images/icons/jobs.svg";
 import company from "../images/icons/company.svg";
 import setting from "../images/icons/setting.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const LeftSideBar = () => {
+    const location = useLocation();
+    const currentUrl = location.pathname;
+
     return (
         <div className="w-[20%] bg-[#FFFFFF] pl-10 h-screen">
             <div className="w-full py-8">
@@ -16,11 +19,11 @@ const LeftSideBar = () => {
             </div>
             <h4 className="my-5 mt-0 text-[18px] text-[#1D2939] font-[500]">MENU</h4>
             <div className="w-4/5 flex flex-col">
-                <Link to='/' className="bg-[#690068] p-[17px] rounded-[16px] text-white flex gap-[16px]">
+                <Link to='/' className={`p-[17px] rounded-[16px] flex gap-[16px] ${currentUrl === '/' ? 'bg-[#690068] text-white' : ''}`}>
                     <img src={dashboard} alt="" />
                     Dashboard
                 </Link>
-                <Link to='/messages' className="p-[17px] rounded-[16px] flex gap-[16px]">
+                <Link to='/messages' className={`p-[17px] rounded-[16px] flex gap-[16px] ${currentUrl === '/messages' ? 'bg-[#690068] text-white' : ''}`}>
                     <img src={chat} alt="" />
                     Messages
                 </Link>
--- src/pages/messages/Messages.js
+++ src/pages/messages/Messages.js
@@ -5,6 +5,8 @@ import avatar1 from "../../images/avatar-1.svg";
 import avatar2 from "../../images/avatar-2.svg";
 import avatar3 from "../../images/avatar-3.svg";
 import avatar4 from "../../images/avatar-4.svg";
+import messagePic from "../../images/message-pic.svg";
+import today from "../../images/today.svg";
 
 const Messages = () => {
     return (
@@ -17,8 +19,8 @@ const Messages = () => {
                 <div className="flex justify-between gap-10">
                     <div className="flex items-center">
                         <button className="bg-[#690068] h-12 rounded-s-2xl p-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-white">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                             </svg>
                         </button>
                         <input type="text" className="h-12 rounded-e-2xl w-60 p-5 placeholder-[#28263B]" placeholder="search messages" />
@@ -31,10 +33,10 @@ const Messages = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex w-full">
-                <div className="w-[35%]">
+            <div className="flex w-full gap-10">
+                <div className="w-[35%] h-[80vh] overflow-auto">
                     <h1 className="my-5">All Messages</h1>
-                    <div className="flex flex-col gap-5">
+                    <div className="flex flex-col gap-2">
                         <div className="bg-[#7CD4FD1A] p-5 flex gap-5 w-full rounded-2xl">
                             <div>
                                 <img src={avatar1} alt="" />
@@ -109,7 +111,60 @@ const Messages = () => {
                         </div>
                     </div>
                 </div>
-                <div className="w-[55%]"></div>
+                <div className="w-[65%]">
+                    <div className="flex bg-white w-full p-5 mt-10 rounded-t-lg justify-between items-center">
+                        <div className="flex gap-8 items-center">
+                            <img src={userAvatar} alt="" />
+                            <h4>Ads for team socials</h4>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+                        </svg>
+
+                    </div>
+                    <div className="bg-[#7CD4FD1A] w-full p-5">
+                        <div className="flex gap-5 my-10 items-center">
+                            <img src={avatar1} alt="" />
+                            <div>
+                                <div className="flex gap-3 items-center mb-3">
+                                    <h4 className="font-[500]">Jane Wilson</h4>
+                                    <span className="text-[14px] text-[#637085]">10:43</span>
+                                </div>
+                                <p className="text-[#28263B] text-[12px] bg-white rounded-xl p-2">Hi Jacob and Brandon, any progress on the project?</p>
+                            </div>
+                        </div>
+                        <div className="flex gap-5 my-10 justify-end">
+                            <div>
+                                <div className="flex gap-3 items-center mb-3 justify-end">
+                                    <h4 className="font-[500]">You</h4>
+                                    <span className="text-[14px] text-[#637085]">10:43</span>
+                                </div>
+                                <div className="text-[#28263B] text-[12px] bg-white rounded-xl p-2">
+                                    <p>
+                                        Hi Jane! Yes.
+                                    </p>
+                                    <p>
+                                        Hi Jane! Yes. I just finished developing the Chat template
+                                    </p>
+                                    <img src={messagePic} alt="" />
+                                </div>
+                            </div>
+                            <img src={avatar4} alt="" />
+                        </div>
+                        <img src={today} alt="" />
+                        <div className="flex gap-5 my-10">
+                            <img src={avatar1} alt="" />
+                            <div>
+                                <div className="flex gap-3 items-center mb-3">
+                                    <h4 className="font-[500]">Jane Wilson</h4>
+                                    <span className="text-[14px] text-[#637085]">10:43</span>
+                                </div>
+                                <p className="text-[#28263B] text-[12px] bg-white rounded-xl p-2">Hi Jacob and Brandon, any progress on the project?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
